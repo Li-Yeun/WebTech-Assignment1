fbb993a19b8d824845ea595610804ef619b9b884
ikbenwasmachine <55793121+ikbenwasmachine@users.noreply.github.com>
2021-04-10 01:38:42 +0200

    Start Assessment
    added being able to pick between topics and quizzes
    somehow broke the checking of the answers :(

--- Assignment 1/java_scripts/assessment.js
+++ Assignment 1/java_scripts/assessment.js
@@ -1,7 +1,7 @@
 //show questions
-function makeQuiz(){
+function makeQuiz(qsts){
     const output = [];
-    myQuestions.forEach((currentQuestion, questionNumber) => {
+    qsts.forEach((currentQuestion, questionNumber) => {
     const possAnswers = [];
     if(currentQuestion.choice){
         for(a in currentQuestion.answers){
@@ -22,18 +22,25 @@ function makeQuiz(){
             );
         }
         output.push(
-            `<div class="question"> ${currentQuestion.question}</div>
-            <div class="answers"> ${possAnswers.join('')}</div><br>`
+            `<br>
+            <div class="title"> Question ${questionNumber + 1}: ${currentQuestion.title}</div>
+            <br>
+            <div class="question"> ${currentQuestion.question}</div>
+            <br>
+            <div class="answers"> ${possAnswers.join('')}</div>
+            <br>
+            <button id="back" onclick=pickTopic()>Back</button> <br>`
         );
     })
     questions.innerHTML = output.join('');
+    questions.innerHTML += (`<br> <button id="submit" onclick=showAnswer(${qsts})>Submit</button>`);
 }
 
-//button
-function showAnswer(){
+// submit button & checking
+function showAnswer(qsts){
     const answers = questions.querySelectorAll('.answers')
     var corrAnswers = 0;
-    myQuestions.forEach((currentQuestion, questionNumber) => {
+    qsts.forEach((currentQuestion, questionNumber) => {
         var givenAnswer;
         const answerNumber = answers[questionNumber];
         if (currentQuestion.choice) {
@@ -44,44 +51,56 @@ function showAnswer(){
             givenAnswer = (document.getElementById(`openQuestion${questionNumber}`) || {}).value;
         }
 
-        if(givenAnswer == currentQuestion.correctAnswer){corrAnswers++;}
+        if(givenAnswer == currentQuestion.correctAnswer){
+            corrAnswers++;
+            answers[questionNumber].style.color = 'green';
+        }
+        else{ answers[questionNumber].style.color = 'red';}
     });
-    result.innerHTML = `<br>You got ${corrAnswers} questions correct!`;
+    result.innerHTML = `<br> You got ${corrAnswers} questions correct!`;
+}
+
+// select the correct topic and quiz
+function pickTopic(){
+    topics = ["Web Browsers", "Safari", "Google Chrome"];
+    topicsContainer = [];
+    topicsContainer.push(`<br> What topic would you like to answer questions about? <br>`);
+    topics.forEach(topic => { topicsContainer.push(`<br><button id="topic" value="${topic}" onclick=pickQuiz(this.value)>${topic}</button><br>`);});
+    questions.innerHTML = topicsContainer.join('');
+}
+function pickQuiz(topic){
+    quizzes = [];
+    if(topic == "Web Browsers"){quizzes = ["History", "Functionality"]};
+    if(topic == "Safari"){quizzes = ["Apple", "Benefits & drawbacks"]};
+    if(topic == "Google Chrome"){quizzes = ["Features"]};
+    // feels like spaghetti
+    quizContainer = [];
+    quizContainer.push(`<br> And what quiz would you like to answer? <br>`);
+    quizzes.forEach(quiz => { quizContainer.push(`<br><button id="quiz" value="${quiz}" onclick=showQuiz(this.value)>${quiz}</button><br>`)});
+    quizContainer.push(`<br> <button id="back" onclick=pickTopic()>Back</button> <br>`);
+    questions.innerHTML = quizContainer.join('');
+}
+
+// show the correct quiz
+function showQuiz(quiz){
+    const thecorrectquestions = myQuestions;  // should be retrieved from DB using quiz
+    makeQuiz(thecorrectquestions);
 }
 
 //var
 const questions = document.getElementById('question');
 const submit = document.getElementById('submit');
 const result = document.getElementById('output');
-const theQuestions = [  // can be removed after it is put on the DB
-    { question:"When was the first actual realease of Google Chrome", 
-    answers: {a:"September 2, 2008", b:"21st night of september", c:"December 11, 2008"}, 
-    correctAnswer:"a",
-    choice:true},
-    { question:"What was Microsoft Edge's code-name?",
-    answers:{a:"Hercules", b:"Chromium", c: "Spartan", d:"Ajax"},
-    correctAnswer:"c",
-    choice:true},
-    { question:"What browser is made for apple devices?",
-    correctAnswer:"Safari",
-    answers: undefined,     // so that .answers gives the same length as theQuestions
-    choice:false},
-    {question:"What is the main villain of an online platforming game?",
-    answers: undefined,
-    correctAnswer:"Browser",
-    choice:false},
-    {question:"Who developed Safari?",
-    answers: {a:"Apple", b:"Steve Jobs", c:"Microsoft", d:"Nvidia"},
-    correctAnswer:"a",
-    choice:true}
-]
-var registeredUser = true; // placeholder, only registered can answer questions.
-const jsonQuestions = '[{ "question":"When was the first actual realease of Google Chrome", "answers": {"a":"September 2, 2008", "b":"21st night of september", "c":"December 11, 2008"}, "correctAnswer":"a","choice":"true"}]';
+var registeredUser = true; 
+    // placeholder, only registered can answer questions.
+const jsonQuestions = '[{ "title":"Release", "question":"When was the first actual realease of Google Chrome", "answers": {"a":"September 2, 2008", "b":"21st night of september", "c":"December 11, 2008"}, "correctAnswer":"a","choice":"true"}]';
     // placeholder for the json input
 var myQuestions = JSON.parse(jsonQuestions);
+    // should be parsing the string asked from the DB with all (and only) the questions that should be shown
+    // otherwise the checking system won't work
+
 if(registeredUser){
-    makeQuiz();
-    submit.addEventListener('click', showAnswer);
+    pickTopic();
 }
 else{
     questions.innerHTML = `ya need ta be registered man.`;
